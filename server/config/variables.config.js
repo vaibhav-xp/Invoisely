cb77a9dbf540c1d32bba395f380f639991bcee26
vaibhavmaurya <vaibhavmaurya@aaraglobal.com>
2024-09-10 21:21:09 +0530

    changed default port server

--- server/config/variables.config.js
+++ server/config/variables.config.js
@@ -4,6 +4,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // global variables 
-export const PORT = process.env.PORT || 8000;
+export const PORT = process.env.PORT || 3000;
 export const URI = process.env.URI || "mongodb://127.0.0.1:27017";
 export const SECRET_ACCESS_TOKEN = process.env.SECRET_ACCESS_TOKEN;
